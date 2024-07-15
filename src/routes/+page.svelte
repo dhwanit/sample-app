16d1d9b9316efba1f7d386a32fe39de4bc7aaff3
Dhwanit Patel <dhwanit@gmail.com>
2024-07-15 22:11:56 +0530

    fixup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
 	import { listen, type UnlistenFn } from "@tauri-apps/api/event";
 	import { onDestroy, onMount } from "svelte";
 
-  const onLogin = () => invoke('login', { apiKey: "", username: "AB0101", password: "R@ndOmP@$$wOrd123!", mfaEncodedSecret: "5BJK5V35KRK5V3OWHJK5ZHIHSNSVTDU3"});
+  const onLogin = () => invoke('login', { apiKey: "Ad4iUn8Aa3i7fwA3q5sJkyUj4o8fjkf", username: "AB0101", password: "R@ndOmP@$$wOrd123!", mfaEncodedSecret: "5BJK5V35KRK5V3OWHJK5ZHIHSNSVTDU3"});
   const onAddMargin = () => invoke('add_margin', { marginAmount: 300000 });
 
   const requestTokenListener = async (e: any) => {
@@ -35,9 +35,9 @@
 
 <div class="max-w-3xl mx-auto">
   <div class="p-2 justify-center items-center flex">
-    <button class="btn btn-md variant-outline-secondary">Login</button>
+    <button class="btn btn-md variant-outline-secondary" on:click={onLogin}>Login</button>
   </div>
   <div class="p-2 justify-center items-center flex h1">
-    <button class="btn btn-md variant-outline-secondary">Add Margin</button>
+    <button class="btn btn-md variant-outline-secondary" on:click={onAddMargin}>Add Margin</button>
   </div>
 </div>
